5ca6b8a72b16c6e2cbd5b388dbedd89d2b540d8b
Alexandr <alex@jurist.vn.ua>
2018-12-11 16:47:39 +0200

    1

--- role.upgrader2.js
+++ role.upgrader2.js
@@ -6,13 +6,13 @@
  * var mod = require('role.upgrader');
  * mod.thing == 'a thing'; // true
  */
-/* 
+
 var definition = require('definition');
 var renew = require('renew');
 var TargetEnergy = require('target.energy');
 var roleUpgrader2 = {
 
-    
+
     run: function(creep) {
             if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
@@ -42,4 +42,3 @@ renew(creep);
 };
 
 module.exports = roleUpgrader2;
-*/
